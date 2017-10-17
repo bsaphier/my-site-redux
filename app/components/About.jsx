d4da08a77323749cf6fe4694d4c7707bf28034e5
Benjamin Saphier <b.saphier@gmail.com>
2017-10-16 23:57:13 -0400

    default commit message

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -18,6 +18,8 @@ const plaxLayers = [
     }
 ];
 
+const siteDesc = `The core of this website is built with React & Redux. The design is my original work but inspired by Google's Material Design. To implement the design, I built a small UI library of React components called react-ssc. The interactive audio (for an example, hover your cursor over the welcome message) is managed by another library I wrote: react-redux-webaudio. Both libraries are available as open-source projects on Github and NPM. In addition to React & Redux, these are some other useful libraries/tools I used to build this site: React-Motion, Webpack, and of course, Babel.`;
+
 
 const About = () => {
     return (
@@ -29,13 +31,25 @@ const About = () => {
             </div>
             <div className={s.cWrap}>
                 <SSC.Card title={'About This Site'}>
-                    {`The core of this website is built with React & Redux. The design is my original work but inspired by Google's Material Design. To implement the design, I built a small UI library of React components called react-ssc. The interactive audio (for an example, hover your cursor over the welcome message) is managed by another library I wrote: react-redux-webaudio. Both libraries are available as open-source projects on Github and NPM. In addition to React & Redux, these are some other useful libraries/tools I used to build this site: React-Motion, Webpack, and of course, Babel.`}
+                    {clicked => (
+                        <div className={s.cContent}>
+                            {clicked ? siteDesc : `${siteDesc.slice(0, 347)}...`}
+                        </div>
+                    )}
                 </SSC.Card>
                 <SSC.Card title={'About Me'}>
-                    {'links n stuff'}
+                    {() => (
+                        <div className={s.cContent}>
+                            {'links n stuff'}
+                        </div>
+                    )}
                 </SSC.Card>
                 <SSC.Card title={'Something Else'}>
-                    {'filler filler filler filler'}
+                    {() => (
+                        <div className={s.cContent}>
+                            {'filler filler filler filler'}
+                        </div>
+                    )}
                 </SSC.Card>
             </div>
         </SSC.PageContent>
