9013978c127a7a186c5a772fcc43ba9a22f129f0
Benjamin Saphier <b.saphier@gmail.com>
2019-06-03 22:10:47 -0400

    copy change

--- app/components/SiteDescription.js
+++ app/components/SiteDescription.js
@@ -10,13 +10,13 @@ function SiteDescription() {
         WELCOME TO MY WEBSITE
       </SSC.Title>
       <p className={s.content}>
-        This is mostly a placeholder but let me tell about what you are looking at (and
-        possibly hearing). To implement the design of this website, I built a small
-        <Link href="https://bsaphier.github.io/simple-site-components/"> library </Link>
-        of <Link href="https://reactjs.org/"> React </Link> components. The interactive audio
-        (for an example, hover your cursor over the welcome message) is being synthesized in your
-        browser using the <Link href="https://webaudio.github.io/web-audio-api/"> web audio
-        api</Link>. Some other useful libraries/tools that I used to build this site were
+        This is mostly a placeholder but let me tell you about what you are looking at.
+        This website is built with a small
+        <Link href="https://bsaphier.github.io/simple-site-components/"> library</Link> of
+        <Link href="https://reactjs.org/"> React </Link>components that I wrote. The interactive
+        audio (for an example, hover your cursor over the welcome message) is being synthesized
+        by your browser using the <Link href="https://webaudio.github.io/web-audio-api/"> web
+        audio api</Link>. Some other useful libraries/tools that I used to build this site were
         <Link href="https://github.com/chenglou/react-motion"> React-Motion</Link>,
         <Link href="https://webpack.js.org/"> Webpack</Link>, and of course,
         <Link href="https://babeljs.io/"> Babel</Link>.
